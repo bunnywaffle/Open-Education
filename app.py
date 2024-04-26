0bbedc53132cbb7791ba9ca3403ac82800567809
Bunnywaffle <100540808+bunnywaffle@users.noreply.github.com>
2024-04-27 01:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ import gradio as gr
 from openai import OpenAI
 
 # Set up the OpenAI API client
-client = OpenAI(base_url="http://localhost:5001/v1", api_key="none")
+client = OpenAI(base_url="http://localhost:5001/v1", api_key="lm-studio")
 
+Prompt = "I am a kind and non-judgmental AI tutor. I will respond in a gentle and empathetic tone, always prioritizing the student's understanding and comfort. I will break down complex concepts into clear, concise, and easy-to-understand language. I will adapt my responses to the student's individual learning style, using a structured and organized approach to facilitate comprehension. I will be patient, encouraging, and supportive in my language, fostering a safe and inclusive learning environment. I will avoid using technical jargon or condescending language, instead focusing on empowering the student with confidence and clarity. I will provide explanations that are relatable, accessible, and engaging, making the learning experience enjoyable and rewarding."
 
 def predict(message, history):
     history_openai_format = []
     for human, assistant in history:
-        history_openai_format.append({"role": "system", "content": "I am a educational tutor" })
+        history_openai_format.append({"role": "system", "content": Prompt })
         history_openai_format.append({"role": "user", "content": human })
         history_openai_format.append({"role": "assistant", "content": assistant})
+    history_openai_format.append({"role": "assistant", "content": Prompt})
     history_openai_format.append({"role": "user", "content": message})
   
     response = client.chat.completions.create(model='gpt-3.5-turbo',
     messages= history_openai_format,
-    temperature=1.0,
+    temperature=0.7,
     stream=True)
 
     partial_message = ""
@@ -26,11 +28,10 @@ def predict(message, history):
 
 gr.ChatInterface(predict,     
     chatbot=gr.Chatbot(height=500),
-    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+    textbox=gr.Textbox(placeholder="Ask Anything I am not a human; I don't judge.", container=False, scale=20),
     title="AI Tutor",
     theme="soft",
     examples=["Help me", "Are tomatoes vegetables?"],
     cache_examples=False,
-    retry_btn=None,
     undo_btn="Delete Previous",
     clear_btn="Clear", submit_btn="Ask",).launch()
